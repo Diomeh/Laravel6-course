4c960df411b59a0363bbbc84a1cad57a86d0c548
Diomeh <duparra15@gmail.com>
2020-08-01 12:05:53 -0400

    Añadido middleware para redireccion de guests

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class ConnectController extends Controller
 {
+
+    public function __construct() {
+        // Prevent users from accessing guest operations
+        $this->middleware('guest')->except(['getLogout']);
+    }
+
     public function getLogin()
     {
     	return view('connect.login');
