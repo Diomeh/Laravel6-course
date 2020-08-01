f91d9b76c6e19fc5f7b756f86744a2fc7625115c
Diomeh <duparra15@gmail.com>
2020-08-01 12:11:43 -0400

    Creada master view para administradores

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,24 @@
+@extends('base')
+
+@section('head')
+	@parent
+	
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<meta name="routeName" content="{{ Route::currentRouteName() }}">
+
+	<link rel="stylesheet" href="{{ url('/static/css/admin.css?v='.time()) }}">
+
+	{{-- Font --}}
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+@stop
+
+@section('content')
+	<div class="wrapper">
+		<div class="col1">
+			@include('admin.sidebar')
+		</div>
+		<div class="col2">
+			
+		</div>
+	</div>
+@stop
