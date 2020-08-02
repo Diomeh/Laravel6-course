beaad8625d18ac7eb7a0f040987c05a64a8c501b
Diomeh <duparra15@gmail.com>
2020-08-02 14:26:29 -0400

    Añadido contenedor principal

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,4 +4,16 @@
 
 @section('content')
 	@parent
+
+	<div class="container-fluid">
+		<div class="panel shadow">
+			<div class="header">
+				<h2 class="title">titulo</h2>
+			</div>
+
+			<div class="inside">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore commodi voluptatum, numquam vitae minus distinctio, ea illum ipsa inventore quas perspiciatis expedita praesentium veniam odit assumenda ratione magni, molestias a.
+			</div>
+		</div>
+	</div>
 @stop
