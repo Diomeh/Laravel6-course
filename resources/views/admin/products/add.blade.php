52d161e54d82187e4de4a35bd24c684df7baaa57
Diomeh <duparra15@gmail.com>
2020-08-02 22:58:30 -0400

    Creado formulario de agregado de productos

---
+++ resources/views/admin/products/add.blade.php
@@ -0,0 +1,99 @@
+@extends('admin.master')
+
+@section('title', 'Añadir Productos')
+
+@section('breadcrumb')
+	@include('breadcrumbs.admin.products.add')	
+@endsection
+
+@section('content-title')
+	<i class="fas fa-boxes"></i>
+	&nbsp Productos - Añadir
+@endsection
+
+@section('content-inside')
+	{!! Form::open(['url' => '/admin/products/add']) !!}
+		<div class="row">
+		    <div class="col-md-6">
+		        <label for="name">Nombre del producto</label>
+		        <div class="input-group">
+		            <div class="input-group-prepend">
+		                <div class="input-group-text"><i class="fas fa-keyboard"></i></div>
+		            </div>
+		            {!! Form::text('name', null, ['class' => 'form-control']) !!}
+		        </div>
+		    </div>
+
+		    <div class="col-md-3">
+		        <label for="name">Categoría</label>
+		        <div class="input-group">
+		            <div class="input-group-prepend">
+		                <div class="input-group-text"><i class="fas fa-tags"></i></div>
+		            </div>
+		            {!! Form::select('category', ['L' => 'Large', 'S' => 'Small'], 'S', ['class' => 'form-control']) !!}
+		        </div>
+		    </div>
+
+		    <div class="col-md-3">
+		        <label for="name">Imagen destacada</label>
+		        <div class="input-group">
+		            <div class="input-group-prepend">
+		                <div class="input-group-text"><i class="fas fa-image"></i></div>
+			            <div class="custom-file">
+			            	{!! Form::file('image', ['class' => 'custom-file-input', 'id' => 'customFile', 'lang' => 'es']) !!}
+							<label class="custom-file-label" for="image">Seleccionar archivo</label>
+						</div>
+		            </div>
+
+		        </div>
+		    </div>
+		</div>
+
+		<div class="row mtop16">
+			<div class="col-md-3">
+				<label for="price">Precio</label>
+				<div class="input-group">
+		            <div class="input-group-prepend">
+		                <div class="input-group-text"><i class="fas fa-dollar-sign"></i></i></div>
+		            </div>
+		            {!! Form::number('price', null, ['class' => 'form-control', 
+		            		'min' => '0.001', 
+		            		'step' => 'any']
+		            	) 
+            		!!}
+		        </div>
+			</div>
+
+			<div class="col-md-3">
+				<label for="discount">Descuento</label>
+					<div class="input-group">
+			            <div class="input-group-prepend">
+			                <div class="input-group-text"><i class="fas fa-dollar-sign"></i></i></div>
+			            </div>
+			            {!! Form::number('discount', null, ['class' => 'form-control', 
+		            			'min' => '0.1', 
+		            			'max' => '100', 
+		            			'step' => 'any']
+		            		) 
+            			!!}
+			        </div>
+				</div>
+			</div>
+
+		<div class="row mtop16">
+			<div class="col-md-12">
+				<label for="description">Descripción</label>
+				{!! Form::textarea('description', null, ['class' => 'form-control']) !!}
+			</div>
+		</div>
+
+		<div class="row mtop16">
+			<div class="col-md-3">
+				<div class="btn-group">
+					{!! Form::submit('Aceptar', ['class' => 'btn btn-primary']) !!}
+					<a href="{{ url('/admin/products') }}" class="btn btn-primary">Cancelar</a>
+				</div>
+			</div>
+		</div>
+	{!! Form::close() !!}
+@endsection
