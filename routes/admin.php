8a137b30c6e0e92cd4dba26a93357fce98a6741a
Diomeh <duparra15@gmail.com>
2020-08-02 22:59:32 -0400

    Añadidas rutas de productos

--- routes/admin.php
+++ routes/admin.php
@@ -5,5 +5,7 @@ Route::prefix('/admin')->group(function(){
 	Route::get('/', 'Admin\DashboardController@getDashboard');
 	Route::get('/users', 'Admin\UserController@getUsers');
 	Route::get('/products', 'Admin\ProductController@getProducts');
+	Route::get('/products', 'Admin\ProductController@index');
+	Route::get('/products/add', 'Admin\ProductController@add');
 });
 ?>
