b29f0d0c1fc233a0273eafb284b2d6781f431c91
Diomeh <duparra15@gmail.com>
2020-09-27 12:49:04 -0400

    Añadidas rutas

--- routes/admin.php
+++ routes/admin.php
@@ -8,5 +8,10 @@ Route::prefix('/admin')->group(function(){
 	// Products
 	Route::get('/products', 'Admin\ProductController@index');
 	Route::get('/products/add', 'Admin\ProductController@add');
+
+	// Categories
+	Route::get('/categories', function(){ return redirect('/admin/categories/0'); });
+	Route::get('/categories/{module}', 'Admin\CategoryController@index');
+	Route::post('/category/add', 'Admin\CategoryController@postCategoryAdd');
 });
 ?>
